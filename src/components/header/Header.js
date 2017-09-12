608bf73493e73bfe53809b7e55d2b7d878949d3c
Saso Sabotin <sasos90@gmail.com>
2017-09-12 12:03:59 +0200

    Simple header created

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="Header">
+                <span>Here is the header.</span>
+            </div>
+        );
+    }
+}
+
+export default Header;
