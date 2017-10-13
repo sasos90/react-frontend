adf1108481b319946c06b6d0e05a6a28ab5b2b8f
Saso Sabotin <sasos90@gmail.com>
2017-10-13 15:45:33 +0200

    Home page created with simple picture

--- src/body/scenes/Home/Home.js
+++ src/body/scenes/Home/Home.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './Home.css';
+import bannerPicture from './img/banner-picture.jpg';
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
-        Home page
+        <div className="startup-wrapper">
+          <div className="banner-wrapper">
+            <img src={bannerPicture} alt=""/>
+          </div>
+          <div className="start-tournament-wrapper">
+            <input type="text" placeholder="Pick your teamname" />
+            <button>Play the tournament</button>
+          </div>
+        </div>
       </div>
     );
   }
