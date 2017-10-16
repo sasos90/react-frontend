0d6f27740a59edccb6c7c7866cee42bac7947d35
Saso Sabotin <sasos90@gmail.com>
2017-10-16 17:20:31 +0200

    Popup with close panel

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -4,8 +4,10 @@ import './Popup.css';
 class Popup extends Component {
   render() {
     return (
-      <div className="Popup" onClick={() => this.props.onClose({closed: true})}>
+      <div className="Popup">
+        <div className="close-panel" onClick={() => this.props.onClose({closed: true})}></div>
         <div className="content-wrapper">
+          <div className="close-button" onClick={() => this.props.onClose({closed: true})}>[X]</div>
           {this.props.children}
         </div>
       </div>
