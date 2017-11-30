c7a2afd07962b310ccc579a57ca201a67c3bb789
Saso Sabotin <sasos90@gmail.com>
2017-11-30 18:19:08 +0100

    Create link for add new player...
    We might need the popup instead

--- src/body/scenes/Team/scenes/Players/TeamPlayers.js
+++ src/body/scenes/Team/scenes/Players/TeamPlayers.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './TeamPlayers.css';
 import players from "../../../../../mock/players";
 import Player from "../../../../components/player/Player";
+import {Link} from "react-router-dom";
 
 class TeamPlayers extends Component {
 
@@ -9,14 +10,16 @@ class TeamPlayers extends Component {
     return (
       <div className="TeamPlayers">
         <div className="players-list">
-          <div className="Player add-new-player">
-            <div className="image-wrapper">
-              <img src={'unknown.png'} alt=""/>
+          <Link to={'/tactics'}>
+            <div className="Player add-new-player">
+              <div className="image-wrapper">
+                <img src={'unknown.png'} alt=""/>
+              </div>
+              <div className="data-wrapper">
+                <div>Add new player</div>
+              </div>
             </div>
-            <div className="data-wrapper">
-              <div>Add new player</div>
-            </div>
-          </div>
+          </Link>
           {this.getTeamPlayers()}
         </div>
       </div>
