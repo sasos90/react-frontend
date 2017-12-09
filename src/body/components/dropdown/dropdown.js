08cc4ef4ea203034c299e3d91ec8e4bb8d2bbfdc
Saso Sabotin <sasos90@gmail.com>
2017-12-09 19:50:19 +0100

    Font for select dropdown applied + some values added to dropdown

--- src/body/components/dropdown/dropdown.js
+++ src/body/components/dropdown/dropdown.js
@@ -8,7 +8,13 @@ class Dropdown extends Component {
       <div className="Dropdown">
         <h3>{this.props.title}</h3>
         <div className="wrapper">
-          Items...
+          <select name="">
+            <option value="-1">Rest</option>
+            <option value="0">Jumping</option>
+            <option value="1">Speed</option>
+            <option value="2">Shooting</option>
+            <option value="3">Technique</option>
+          </select>
         </div>
       </div>
     );
